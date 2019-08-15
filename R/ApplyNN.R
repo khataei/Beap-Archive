999adaa01dc08b2f503085986d2938a73c2ba99e
Javad-mun <skhataeipour@mun.ca>
2019-08-15 15:35:37 -0230

    Init commit of thenew function for NN

---
+++ R/ApplyNN.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param raw_df input data
+#' @param n_var number of predictors. i, e. independent variables
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ApplyNN <- function(raw_df , n_var){
+
+}
