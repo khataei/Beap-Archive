180a7000d4effd34adc18d00455bf64a6e518142
Javad-mun <skhataeipour@mun.ca>
2019-09-04 16:41:50 -0230

    it was a draft

--- R/overlap.R
+++
@@ -1,8 +0,0 @@
-# win_size = 5
-# overlap = 0
-# distance = win_size - overlap
-#
-# results <- zoo::rollapply( data = roll_data,
-#                 width = win_size,
-#                 by = distance,
-#                 FUN = sum) %>%  data.frame()
